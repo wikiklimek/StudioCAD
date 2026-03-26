8e447e24154f04e5a6af805ec5f2e78efa7d2ab2
Wiktoria Klimek <wikiklimek1@gmail.com>
2026-03-26 20:26:41 +0100

    dziala przed dodaniem gui z parametrami

--- Project/src/shapes/headers/cursor.h
+++ Project/src/shapes/headers/cursor.h
@@ -14,39 +14,12 @@ public:
     unsigned int VAO=0, VBO=0;
 
 
-    ~Cursor()
-    {
-        if (VAO) glDeleteVertexArrays(1, &VAO);
-        if (VBO) glDeleteBuffers(1, &VBO);
-    }
+    ~Cursor();
 
-    void Init()
-    {
-        float lines[] =
-                {
-                -1.0f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f, // X
-                0.0f,-1.0f, 0.0f,  0.0f, 1.0f, 0.0f, // Y
-                0.0f, 0.0f,-1.0f,  0.0f, 0.0f, 1.0f  // Z
-        };
-        glGenVertexArrays(1, &VAO);
-        glGenBuffers(1, &VBO);
-        glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(lines), lines, GL_STATIC_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-    }
+    void Init();
 
-    void Draw(Shader& shader)
-    {
-        //Mat4 modelMat = createModelMatrix(transform.getPosition(), transform.getRotation(), transform.scale);
-        Mat4 modelMat = createModelMatrix(transform.getPosition(), transform.rotation, transform.scale);
-        glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, modelMat.table);
-        float color[3] = { 1.0f, 0.0f, 1.0f }; // Różowy kursor dla odróżnienia [cite: 7]
-        glUniform3fv(glGetUniformLocation(shader.ID, "objectColor"), 1, color);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_LINES, 0, 6);
-    }
+
+    void Draw(Shader& shader);
 };
 
 // Zmienna globalna lub wewnątrz int main()
---
+++ Project/src/shapes/source/cursor.cpp
@@ -0,0 +1,48 @@
+#pragma once
+#include "cursor.h"
+
+Cursor::~Cursor()
+{
+    if (VAO) glDeleteVertexArrays(1, &VAO);
+    if (VBO) glDeleteBuffers(1, &VBO);
+}
+
+void Cursor::Init()
+{
+    float lines[] =
+            {
+                    -1.0f, 0.0f, 0.0f,  1.0f, 0.0f, 0.0f, // X
+                    0.0f,-1.0f, 0.0f,  0.0f, 1.0f, 0.0f, // Y
+                    0.0f, 0.0f,-1.0f,  0.0f, 0.0f, 1.0f  // Z
+            };
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(lines), lines, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+}
+
+
+void Cursor::Draw(Shader& shader)
+{
+    Mat4 modelMat = createModelMatrix(transform.getPosition(), transform.rotation, transform.scale);
+    glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, modelMat.table);
+    glBindVertexArray(VAO);
+
+    // Oś X - Czerwona (wierzchołki 0 i 1)
+    float colorX[3] = { 1.0f, 0.0f, 0.0f };
+    glUniform3fv(glGetUniformLocation(shader.ID, "objectColor"), 1, colorX);
+    glDrawArrays(GL_LINES, 0, 2);
+
+    // Oś Y - Zielona (wierzchołki 2 i 3)
+    float colorY[3] = { 0.0f, 1.0f, 0.0f };
+    glUniform3fv(glGetUniformLocation(shader.ID, "objectColor"), 1, colorY);
+    glDrawArrays(GL_LINES, 2, 2);
+
+    // Oś Z - Niebieska (wierzchołki 4 i 5)
+    float colorZ[3] = { 0.0f, 0.0f, 1.0f };
+    glUniform3fv(glGetUniformLocation(shader.ID, "objectColor"), 1, colorZ);
+    glDrawArrays(GL_LINES, 4, 2);
+}
